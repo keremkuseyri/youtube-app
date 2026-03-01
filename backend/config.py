f323e4c67d6c7f997a5c9b9ab21961f9bfe83fe8
kerem.kuseyri <kerem.kuseyri@gmail.com>
2026-03-01 20:53:33 +0300

    groq api

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     YT_API_KEY = os.getenv("YT_API_KEY", "AIzaSyCe_POTISdJFdUeSuKNUP7_MQFTmqwPUbE")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyAnRiOeJ598q3ZxaU-dKkXwAXz_0RZgqco")
+    GROQ_API_KEY =os.getenv("GROQ_API_KEY", "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")
     AI_MODEL = os.getenv("AI_MODEL", "models/gemini-flash-lite-latest")
     AI_FALLBACK_MODELS = [
         model.strip()
